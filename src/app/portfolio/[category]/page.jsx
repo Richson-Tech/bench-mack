07b9f47cbe5967e038550480e1d93b043e281635
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-08-11 18:54:03 +0100

    commit new changes

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
 const category = ({ params }) => {
   return (
     <div>
-      <h1>{params.category}</h1>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#"/>
+        </div>
+        <div className={styles.container}>
+         <Image className={styles.img} fill={true} src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG9tZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" alt=""/> 
+        </div>
+      </div>
     </div>
   );
 };
